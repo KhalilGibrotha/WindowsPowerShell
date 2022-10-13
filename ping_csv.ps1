a312a143474d3fbc7e053bbac31204610f8753ce
KhalilGibrotha <Alexgambino22@gmail.com>
2022-10-13 11:21:04 -0400

    Fixed the object property for the devicename

--- ping_csv.ps1
+++ ping_csv.ps1
@@ -5,30 +5,30 @@ function Get-ServerEngInfo ($csvImport)
     if (!($csvImport)) 
     {
         # Read the Name Column from the CSV file
-        $csvImport = Import-Csv .\Test.csv
+        $csvImport = Import-Csv ..\Test.csv
     }
  
     # For Each Computer Name perform Ping Test
     $results = foreach ($computer in $csvImport)
     {
-        $computerName = $computer.Name
+        $computerName = $computer.deviceHostname
         if (Test-Connection $computerName -Count 1)
         { 
             $address = (Test-Connection $computerName -Count 1 -ErrorAction SilentlyContinue).IPV4Address.ipaddresstostring
         }
         else
         {
-            $address = "Offline"
+            $address = "NoProdICMP"
         }
         [PSCustomObject]@{
-        Computer = $computerName
-        Address = $address}
+        deviceHostname = $computerName
+        IPv4Address = $address}
     }
 # Sample outputs - pick one or more, your choice
-$results
+#$results
 $results | Format-Table -AutoSize
-$results | Out-GridView
+#$results | Out-GridView
 $results | Export-Csv -Path .\foo.csv -NoTypeInformation -Encoding ASCII
-$results | Out-File -FilePath .\foo.txt -Encoding ASCI
-$results | Export-Clixml -Path .\foo.xml -Encoding ASCII
+#$results | Out-File -FilePath .\foo.txt -Encoding ASCI
+#$results | Export-Clixml -Path .\foo.xml -Encoding ASCII
 }
